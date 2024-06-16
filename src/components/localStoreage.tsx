4aa504ef538b3f62a743ec5bdbd26e83b1712c9f
enterpannet <enterpannet@gmail.com>
2024-06-16 20:59:43 +0700

    add font

--- src/components/localStoreage.tsx
+++ src/components/localStoreage.tsx
@@ -8,130 +8,133 @@ import { questionsData, questionsState } from '@/recoil/questionsState';
 import { RetirementPlanData, retirementPlanState } from "@/recoil/retirementPlanState";
 
 interface HealthPlanStorageData {
-    hospitals?: string;
-    dailyCompensationFromWelfare?: string;
-    treatingSeriousIllness?: string;
-    emergencyCosts?: string;
-    annualTreatment?: string;
-    roomFeeFromCompany?: string;
-    dailyCompensationFromCompany?: string;
-    treatingSeriousIllnessFromCompany?: string;
-    emergencyCostsFromCompany?: string;
-    annualTreatmentFromCompany?: string;
+  hospitals?: string;
+  dailyCompensationFromWelfare?: string;
+  treatingSeriousIllness?: string;
+  emergencyCosts?: string;
+  annualTreatment?: string;
+  roomFeeFromCompany?: string;
+  dailyCompensationFromCompany?: string;
+  treatingSeriousIllnessFromCompany?: string;
+  emergencyCostsFromCompany?: string;
+  annualTreatmentFromCompany?: string;
 }
 interface EducationPlanStorageData {
-    levelOfeducation: string;
-    typeOfeducation: string;
-    yearsOfeducation: string;
-    inflationRate: string;
-    deposit: string;
-    insuranceFund: string;
-    otherAssets: string;
+  levelOfeducation: string;
+  typeOfeducation: string;
+  yearsOfeducation: string;
+  inflationRate: string;
+  deposit: string;
+  insuranceFund: string;
+  otherAssets: string;
 }
 interface ProtectionPlanStorageData {
-    initialMonthlyExpense: string
-    numberOfYears: string
-    adjustedYearlyExpenses: string
-    inflationRate: string
-    homePayments: string
-    carPayments: string
-    otherDebts: string
-    bankDeposit: string
-    lifeInsuranceFund: string
-    otherAssets: string
+  initialMonthlyExpense: string
+  numberOfYears: string
+  adjustedYearlyExpenses: string
+  inflationRate: string
+  homePayments: string
+  carPayments: string
+  otherDebts: string
+  bankDeposit: string
+  lifeInsuranceFund: string
+  otherAssets: string
 }
 interface retirementPlanStorageData {
-    livingCosts: string
-    houseCosts: string
-    internetCosts: string
-    clothingCosts: string
-    medicalCosts: string
-    otherCosts: string
-    age: string
-    retireAge: string
-    lifExpectancy: string
-    inflationRate: string
-    deposit: string
-    insuranceFund: string
-    otherAssets: string
+  livingCosts: string
+  houseCosts: string
+  internetCosts: string
+  clothingCosts: string
+  medicalCosts: string
+  otherCosts: string
+  age: string
+  retireAge: string
+  lifExpectancy: string
+  inflationRate: string
+  deposit: string
+  insuranceFund: string
+  otherAssets: string
 }
 export interface nameData {
-    user_params: string;
-    nickname?: string;
-    age?: string;
+  user_params: string;
+  nickname?: string;
+  age?: string;
 }
 export const useLocalStorage = () => {
-    const [name, setName] = useRecoilState(nameState);
-    const [educationPlan, setEducationPlan] = useRecoilState(educationPlanState);
-    const [healthPlan, setHealthPlan] = useRecoilState(healthPlanState);
-    const [protection, setProtection] = useRecoilState(protectionPlanState);
-    const [questions, setQuestions] = useRecoilState(questionsState);
-    const [retirement, setRetirement] = useRecoilState(retirementPlanState);
+  const [name, setName] = useRecoilState(nameState);
+  const [educationPlan, setEducationPlan] = useRecoilState(educationPlanState);
+  const [healthPlan, setHealthPlan] = useRecoilState(healthPlanState);
+  const [protection, setProtection] = useRecoilState(protectionPlanState);
+  const [questions, setQuestions] = useRecoilState(questionsState);
+  const [retirement, setRetirement] = useRecoilState(retirementPlanState);
 
-    const loadData = () => {
-        const storedData = localStorage.getItem(name.user_params);
-        if (storedData) {
-          const parsedData: {
-            nickname: string;
-            age: string;
-            educationPlan?: EducationPlanStorageData;
-            healthPlan?: HealthPlanStorageData;
-            protection?: ProtectionPlanStorageData;
-            questions?: questionsData;
-            retirement?: retirementPlanStorageData;
-          } = JSON.parse(storedData);
-    
-          if (
-            parsedData.nickname === name.nickname &&
-            parsedData.age === name.age
-          ) {
-            if (parsedData.educationPlan) {
-              setEducationPlan((preveducationPlan) => ({
-                ...preveducationPlan,
-                ...parsedData.educationPlan,
-              }));
-            }
-            if (parsedData.healthPlan) {
-              setHealthPlan((prevHealthPlan) => ({
-                ...prevHealthPlan,
-                ...parsedData.healthPlan,
-              }));
-            }
-            if (parsedData.protection) {
-              setProtection((prevprotection) => ({
-                ...prevprotection,
-                ...parsedData.protection,
-              }));
-            }
-            if (parsedData.questions) {
-              setQuestions(parsedData.questions);
-            }
-            if (parsedData.retirement) {
-              setRetirement((prevretirement) => ({
-                ...prevretirement,
-                ...parsedData.retirement,
-              }));
-            }
-          } else {
-            localStorage.removeItem(name.user_params);
-          }
-        }
-      };
-
-    const saveData = (dataToSave: {
-        nickname?: string;
-        age?: string;
+  const loadData = () => {
+    const storedData = localStorage.getItem(name.user_params);
+    if (storedData) {
+      const parsedData: {
+        nickname: string;
+        age: string;
         educationPlan?: EducationPlanStorageData;
         healthPlan?: HealthPlanStorageData;
         protection?: ProtectionPlanStorageData;
         questions?: questionsData;
         retirement?: retirementPlanStorageData;
-    }) => {
-        const userData = {
-            ...dataToSave,
-        };
-        localStorage.setItem(name.user_params, JSON.stringify(userData));
-    };
+      } = JSON.parse(storedData);
 
-    return { saveData ,loadData};
+      if (
+        parsedData.nickname === name.nickname &&
+        parsedData.age === name.age
+      ) {
+        if (parsedData.educationPlan) {
+          setEducationPlan((preveducationPlan) => ({
+            ...preveducationPlan,
+            ...parsedData.educationPlan,
+          }));
+        }
+        if (parsedData.healthPlan) {
+          setHealthPlan((prevHealthPlan) => ({
+            ...prevHealthPlan,
+            ...parsedData.healthPlan,
+          }));
+        }
+        if (parsedData.protection) {
+          setProtection((prevprotection) => ({
+            ...prevprotection,
+            ...parsedData.protection,
+          }));
+        }
+        if (parsedData.questions) {
+          setQuestions(parsedData.questions);
+        }
+        if (parsedData.retirement) {
+          setRetirement((prevretirement) => ({
+            ...prevretirement,
+            ...parsedData.retirement,
+          }));
+        }
+      } else {
+        localStorage.removeItem(name.user_params);
+      }
+    }
+    const PlanStore = localStorage.getItem('saveEducationplan')
+    console.log(PlanStore);
+
+  };
+
+  const saveData = (dataToSave: {
+    nickname?: string;
+    age?: string;
+    educationPlan?: EducationPlanStorageData;
+    healthPlan?: HealthPlanStorageData;
+    protection?: ProtectionPlanStorageData;
+    questions?: questionsData;
+    retirement?: retirementPlanStorageData;
+  }) => {
+    const userData = {
+      ...dataToSave,
+    };
+    localStorage.setItem(name.user_params, JSON.stringify(userData));
+  };
+
+  return { saveData, loadData };
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RecoilRoot } from "recoil";
-// import { BrowserRouter } from "react-router-dom";
-
-// import "./assets/global.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RecoilRoot>
-      {/* <BrowserRouter basename={"/financial-health-check/"}> */}
-      <RouterProvider router={router} />
-      {/* </BrowserRouter> */}
-    </RecoilRoot>
+    <div className="font-new-allianz-neo">
+      <RecoilRoot>
+        <RouterProvider router={router} />
+      </RecoilRoot>
+    </div>
   </React.StrictMode>
 );
